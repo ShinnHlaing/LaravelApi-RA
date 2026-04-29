f3fc1d6e506161f8925f28b98e56b6f605c1702a
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-04-29 22:52:08 +0630

    add exceptional handling to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,46 +3,66 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $post = Post::all();
-        return response()->json(['message' => 'Posts retrieved successfully!', 'data' => $post]);
+        try {
+            $post = Post::all();
+            return response()->json(['message' => 'Posts retrieved successfully!', 'data' => $post]);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Sever error!']);
+        }
     }
 
     public function show(int $id)
     {
-        $post = Post::find($id);
-        return response()->json(['message' => 'Post retrieved successfully!', 'data' => $post]);
+        try {
+            $post = Post::find($id);
+            return response()->json(['message' => 'Post retrieved successfully!', 'data' => $post], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Sever error!'], 500);
+        }
     }
 
     public function store(Request $request)
     {
+        try {
+            Post::create([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
 
-        Post::create([
-            'title' => $request->title,
-            'content' => $request->content
-        ]);
-
-        return response()->json(['message' => 'Post created successfully!']);
+            return response()->json(['message' => 'Post created successfully!'], 201);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Sever error!'], 500);
+        }
     }
 
     public function update(Request $request, int $id)
     {
-        $post = Post::find($id);
-        $post->title = $request->title;
-        $post->content = $request->content;
-        $post->save();
-        return response()->json(['message' => 'Post updated successfully!']);
+        try {
+            $post = Post::find($id);
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->save();
+            return response()->json(['message' => 'Post updated successfully!'], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Sever error!'], 500);
+        }
     }
 
     public function destroy(int $id)
     {
-        $post = Post::find($id);
-        $post->delete();
-        return response()->json(['message' => 'Post deleted successfully!']);
+        try {
+            $post = Post::find($id);
+            $post->delete();
+            return response()->json(['message' => 'Post deleted successfully!'], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Sever error!'], 500);
+        }
     }
 }
