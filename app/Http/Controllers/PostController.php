f0c6683d88f686002351aaf93dc87e2a654d3809
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-04-29 23:10:12 +0630

    create PostResource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Exception;
 use Illuminate\Http\Request;
@@ -12,7 +13,7 @@ class PostController extends Controller
     {
         try {
             $post = Post::all();
-            return response()->json(['message' => 'Posts retrieved successfully!', 'data' => $post]);
+            return response()->json(['message' => 'Posts retrieved successfully!', 'data' => PostResource::collection($post)]);
         } catch (Exception $e) {
             return response()->json(['message' => 'Sever error!']);
         }
@@ -22,7 +23,7 @@ class PostController extends Controller
     {
         try {
             $post = Post::find($id);
-            return response()->json(['message' => 'Post retrieved successfully!', 'data' => $post], 200);
+            return response()->json(['message' => 'Post retrieved successfully!', 'data' => PostResource::make($post)], 200);
         } catch (Exception $e) {
             return response()->json(['message' => 'Sever error!'], 500);
         }
@@ -31,12 +32,12 @@ class PostController extends Controller
     public function store(Request $request)
     {
         try {
-            Post::create([
+            $post = Post::create([
                 'title' => $request->title,
                 'content' => $request->content
             ]);
 
-            return response()->json(['message' => 'Post created successfully!'], 201);
+            return response()->json(['message' => 'Post created successfully!', 'data' => PostResource::make($post)], 201);
         } catch (Exception $e) {
             return response()->json(['message' => 'Sever error!'], 500);
         }
@@ -49,7 +50,7 @@ class PostController extends Controller
             $post->title = $request->title;
             $post->content = $request->content;
             $post->save();
-            return response()->json(['message' => 'Post updated successfully!'], 200);
+            return response()->json(['message' => 'Post updated successfully!', 'data' => PostResource::make($post)], 200);
         } catch (Exception $e) {
             return response()->json(['message' => 'Sever error!'], 500);
         }
@@ -60,7 +61,7 @@ class PostController extends Controller
         try {
             $post = Post::find($id);
             $post->delete();
-            return response()->json(['message' => 'Post deleted successfully!'], 200);
+            return response()->json(['message' => 'Post deleted successfully!', 'data' => null], 200);
         } catch (Exception $e) {
             return response()->json(['message' => 'Sever error!'], 500);
         }
